459f4162541b5b482e08fe19ed6a06898a52f47c
zSInasa13 <zSInasa13@hatboro-horsham.org>
2025-03-11 10:08:35 -0400

    FINISHED

--- TextBasedRPG.java
+++ TextBasedRPG.java
@@ -60,7 +60,7 @@ public class TextBasedRPG {
             String choice = scan.nextLine();
             if (choice.equals("yes")) {
                 typewriter("Your computer explodes and you die.");
-                System.out.println("Mission Failed.");
+                typewriter("Mission Failed.");
             } else {
                 typewriter("You decide to investigate more and learn that it would’ve exploded the computer if opened,");
                 typewriter("after some looking you find that it was from a anonymous source,");
@@ -82,7 +82,7 @@ public class TextBasedRPG {
         } else {
             typewriter("You forget about it and go get a doughnut and coffee, while getting your doughnut you drop your coffee, slip on it,");
             typewriter("landing on your back paralyzing yourself from the waist down.");
-            System.out.println("Mission Failed.");
+            typewriter("Mission Failed.");
 
         }
 
@@ -143,7 +143,7 @@ public class TextBasedRPG {
             typewriter("You go in and see some filing cabinets and a desk in the middle of the room. ");
             typewriter("While getting closer to the desk you get hit on the back of the head.");
             typewriter("You turn around and he hits you again and knocks you out...");
-            typewriter("To be continued...");
+            situationTwo();
         }
 
     }
@@ -157,7 +157,12 @@ public class TextBasedRPG {
         } else {
             typewriter("You wake up in a dark dimly lit room, tied up and confused you try to find a way to cut your binds,");
             typewriter("before you can further examine Danny Wax walks in the room, holding a full glass of red wine.");
-            typewriter("To be continued...");
+            typewriter("CEO: I wish we could’ve in a better place, but it seems this will do just fin-");
+            typewriter("As he’s finishing, he drops his glass, he sighs and leaves the room.");
+            typewriter("");
+            typewriter("");
+            typewriter("You have a chance to kick some glass to gur leg and possibly cut the rope ");
+            tenthChoice();
         }
 
     }
@@ -227,13 +232,164 @@ public class TextBasedRPG {
     }
 
 
+
+    public static void tenthChoice() {
+        typewriter("Do you go for the glass? (yes/no):");
+        String choice = scan.nextLine();
+        if (choice.equals("yes")) {
+            typewriter("With your hands behind your back you grab the glass and try to start cutting");
+            luckTwo();
+        } else {
+            typewriter("Danny Wax walks in with a candle and a cloth, while cleaning it up");
+            typewriter("up the candle randomly falls onto the wine burning the both of you.");
+            typewriter("Mission Succfailed");
+
+        }
+
+    }
+
+
+    public static void luckTwo() {
+        int num = random.nextInt(0,11);
+
+        if (num > 5)
+
+        {
+            typewriter("You cut the rope right before he comes in, as he walks in you grab him and end him with the glass.");
+            typewriter("You grab a Jar a pure golden honey he had in his bag,");
+            typewriter("you frantically look for a way out of this complex you’re in,");
+            typewriter("after running through hallways you see a door that leads to sun light.");
+            eleventhChoice();
+        }
+        else
+        {
+            typewriter("You can’t mange to cut the rope, Danny Wax comes backs in and sees your escape attempt...");
+            typewriter("He shoots and kills you.");
+            typewriter("Mission Failed.");
+
+        }
+
+
+    }
+
+
+
+
+    public static void eleventhChoice() {
+        typewriter("Do you go for it? (yes/no):");
+        String choice = scan.nextLine();
+        if (choice.equals("yes")) {
+            typewriter("as you run towards it you get shot in the side by a Beefy man coming from the room you had previously ran out of,");
+            typewriter("you run into the snowy forest looking to get as far away as you can.");
+            typewriter("After getting far into the forest you sit against a log in the snow waiting to perish to your wounds,");
+            typewriter("while waiting you seem a small golden bear wearing red shirt,");
+            typewriter("he walks up to you and asks to try your honey");
+            twelveChoice();
+        } else {
+            typewriter("You hesitate and the Beefy man comes out from the room you were previously in and shoots you.");
+            typewriter("You die.");
+            typewriter("Mission Failed.");
+
+        }
+
+    }
+
+
+    public static void twelveChoice() {
+        typewriter("Do you let him try the honey? (yes/no): ");
+        String choice = scan.nextLine();
+        if (choice.equals("yes")) {
+            typewriter("You give him the Jar of Golden Honey, he grabs a handful and stuffs it in his mouth,");
+            typewriter("after chewing for awhile he looks at you and nods, he then grabs another handful and rubs it on your wound,");
+            typewriter("he stands up smiles at you, and walks Jar of Honey in hand.");
+            typewriter("Mission Complete");
+        } else {
+            typewriter("The Golden Bear walks away and you did nothing as you’ll eventually perish...");
+            typewriter("But instead, you become one with the log you were laying up against,");
+            typewriter("with the Jar of honey next to you over many many years you will grow into a great Honey Tree,");
+            typewriter("and the Golden Bear in the Red shirt will come collect his honey.");
+            typewriter("Mission Honeyed");
+        }
+
+
+
+    }
+
+
+
+
+    public static void situationTwo() {
+        typewriter("You wake up in a dark dimly lit room, tied up and confused you try to find a way to cut your binds,");
+        typewriter("before you can further examine Danny Wax walks in the room, holding a full glass of red wine.");
+        typewriter("CEO: I wish we could’ve in a better place, but it seems this will do just fin-");
+        typewriter("As he’s finishing, he drops his glass, he sighs and leaves the room.");
+        typewriter("");
+        typewriter("");
+        typewriter("You have a chance to kick some glass to gur leg and possibly cut the rope ");
+        tenthChoice();
+    }
+
+
+
     public static void situationOne() {
         typewriter("After getting home, , in this file you find out that BeezWax could’ve gotten a small group ");
         typewriter("of the Bees and brought them here to breed and make honey, but it costs more to build and maintain that farm ");
         typewriter("than to import the honey from a poor country, but also from this file you find what seems ");
         typewriter("to be Danny Wax’s address, you must go.");
-        typewriter("To be continued...");
+        typewriter("");
+        typewriter("Friday:");
+        typewriter("");
+        typewriter("Sitting in your car outside Danny’s house you think that driving in might be easier than walking in.");
+        thirteenthChoice();
     }
+
+
+
+
+    public static void thirteenthChoice() {
+        typewriter("Drive in or Walk in? (Drive in/Walk in):");
+        String choice = scan.nextLine();
+        if (choice.equals("Drive in")) {
+            typewriter("You’re Driving your car into Danny Wax’s house at full speed!");
+            typewriter("The last thing you see before your car explodes the house is Danny Wax and his family eating dinner with a side of honey.");
+            typewriter("Mission complete.");
+        } else {
+            typewriter("You walk in the house’s front door gun in hand.");
+            typewriter("Walking in you see Danny Wax with his family sitting at the dinner table,");
+            typewriter("he looks at you for awhile in shock before he offers you a seat in an empty chair.");
+            fourteenthChoice();
+        }
+
+    }
+
+
+
+
+
+    public static void fourteenthChoice() {
+        typewriter("Shoot or Sit? (Shoot/Sit):");
+        String choice = scan.nextLine();
+        if (choice.equals("Shoot")) {
+            typewriter("You shoot Danny in front of his family and go to walk out of the house but before you do you grab a Jar of Golden Honey.");
+            typewriter("You go home and give it to your pet squirrel “Ronnie");
+            typewriter("You and Ronnie watch Alvin and the chipmunks while eating honey toast.");
+        } else {
+            typewriter("You sit down and an old lady puts some food on your plate, after talking to Danny ad his family for awhile you learn they had");
+            typewriter("no idea of the hurt it causes and would prefer if Danny had farms or it here,");
+            typewriter("after talking to them some more Danny agreed to move honey productions to farms here.");
+            typewriter("");
+            typewriter("After eating some more your start to feel weird, you look up and the whole family is looking at you,");
+            typewriter("they poisoned you, you try to stand but you just fall over.");
+            typewriter("The poison takes over and you die.");
+            typewriter("Mission failed.");
+
+        }
+
+    }
+
+
+
+
 
 
     public static void typewriter(String text) {
